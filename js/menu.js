f8b6536a91347c117137a3eca9734e9d5e74483a
Michel Echavarria <mechavaa@triplew.cl>
2021-12-13 20:14:10 -0300

    Actualización final

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,7 @@
 let burguerButton = document.getElementById('burguer-button');
 let menu = document.getElementById('menu');
+console.log(menu);
 
-// burguerButton contiene el span con el icono
-// burguerButton.addEventListener('que evento', 'que deseo hacer');
 burguerButton.addEventListener('touchstart', function() {
   menu.classList.toggle('active');
 });
